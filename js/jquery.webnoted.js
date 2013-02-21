701622fb9b89e1aaf0bcfa15d6622ed5627695eb
Michael Clifford <cliffom@gmail.com>
2013-02-20 21:37:54 -0800

    JS Cleanups

--- js/jquery.webnoted.js
+++ js/jquery.webnoted.js
@@ -7,174 +7,174 @@
  * or GPL Version 2 (http://www.opensource.org/licenses/gpl-2.0.php) licenses.
  *
  */
-(function($) {
-	var apiURL;
-	var dataStore;
-	var canSave = true;
-	var noteId;
-	var settings;
-	var sharedUrl;
-	var version = '1.0.7';
-	var webNoted;
+(function ($) {
+    var apiURL;
+    var dataStore;
+    var canSave = true;
+    var noteId;
+    var settings;
+    var sharedUrl;
+    var version = '1.0.7';
+    var webNoted;
 
-	var methods = {
-		init: function(options) {
-			webNoted = this;
+    var methods = {
+        init:function (options) {
+            webNoted = this;
 
-			settings = $.extend({
-				'apiURL': '/api/',
-				'dataStore': localStorage,
-				'noteId': ''
-			}, options);
+            settings = $.extend({
+                'apiURL':'/api/',
+                'dataStore':localStorage,
+                'noteId':''
+            }, options);
 
-			apiURL = options['apiURL'];
-			dataStore = options['dataStore'];
-			noteId = options['noteId'];
+            apiURL = options['apiURL'];
+            dataStore = options['dataStore'];
+            noteId = options['noteId'];
 
-			if (noteId !== '') {
-				canSave = false;
-			} else {
-				this.webNoted('open');
-				this.webNoted('setEditable');
-				this.webNoted('save');
-			}
+            if (noteId !== '') {
+                canSave = false;
+            } else {
+                this.webNoted('open');
+                this.webNoted('setEditable');
+                this.webNoted('save');
+            }
 
-			this.on('keyup paste', function() {
-				webNoted.trigger('contentChanged');
-			});
+            this.on('keyup paste', function () {
+                webNoted.trigger('contentChanged');
+            });
 
-			return this;
-		},
+            return this;
+        },
 
-		save: function() {
-			if (canSave) {
-				dataStore.setItem(webNoted.webNoted('getCurrentDocument'), webNoted.webNoted('getContents'));
-			}
-			return this;
-		},
+        save:function () {
+            if (canSave) {
+                dataStore.setItem(webNoted.webNoted('getCurrentDocument'), webNoted.webNoted('getContents'));
+            }
+            return this;
+        },
 
-		open: function() {
-			webNoted.webNoted('setContents', dataStore.getItem(webNoted.webNoted('getCurrentDocument')));
-			return this;
-		},
+        open:function () {
+            webNoted.webNoted('setContents', dataStore.getItem(webNoted.webNoted('getCurrentDocument')));
+            return this;
+        },
 
-		edit: function() {
-			webNoted.webNoted('setCurrentDocument', webNoted.webNoted('getNewNoteName'));
-			canSave = true;
-			webNoted.webNoted('save');
-			window.location = '/';
-		},
+        edit:function () {
+            webNoted.webNoted('setCurrentDocument', webNoted.webNoted('getNewNoteName'));
+            canSave = true;
+            webNoted.webNoted('save');
+            window.location = '/';
+        },
 
-		create: function() {
-			webNoted.webNoted('save');
-			webNoted.webNoted('clear');
-			webNoted.webNoted('setCurrentDocument', webNoted.webNoted('getNewNoteName'));
-			webNoted.webNoted('save');
-			webNoted.trigger('noteCreated');
-		},
+        create:function () {
+            webNoted.webNoted('save');
+            webNoted.webNoted('clear');
+            webNoted.webNoted('setCurrentDocument', webNoted.webNoted('getNewNoteName'));
+            webNoted.webNoted('save');
+            webNoted.trigger('noteCreated');
+        },
 
-		switchDocument: function(documentName) {
-			webNoted.webNoted('save');
-			webNoted.webNoted('setCurrentDocument', documentName);
-			webNoted.webNoted('open');
-		},
+        switchDocument:function (documentName) {
+            webNoted.webNoted('save');
+            webNoted.webNoted('setCurrentDocument', documentName);
+            webNoted.webNoted('open');
+        },
 
-		clear: function() {
-			webNoted.webNoted('setContents', '');
-			return this;
-		},
+        clear:function () {
+            webNoted.webNoted('setContents', '');
+            return this;
+        },
 
-		share: function() {
-			$.ajax({
-				url: apiURL,
-				type: 'post',
-				data: {
-					note: webNoted.webNoted('getContents')
-				},
-				dataType: 'json',
-				error: function(jqXHR, textStatus, errorThrown) {
-					webNoted.trigger('shareLinkError');
-				}
-			}).done(function(msg) {
-				try {
-					if (msg.status === 'success') {
-						sharedUrl = msg.sharedUrl;
-						webNoted.trigger('shareLinkGenerated');
-					} else {
-						webNoted.trigger('shareLinkError');
-					}
-				} catch (e) {
-					webNoted.trigger('shareLinkError');
-				}
-			});
-			return this;
-		},
+        share:function () {
+            $.ajax({
+                url:apiURL,
+                type:'post',
+                data:{
+                    note:webNoted.webNoted('getContents')
+                },
+                dataType:'json',
+                error:function (jqXHR, textStatus, errorThrown) {
+                    webNoted.trigger('shareLinkError');
+                }
+            }).done(function (msg) {
+                    try {
+                        if (msg.status === 'success') {
+                            sharedUrl = msg.sharedUrl;
+                            webNoted.trigger('shareLinkGenerated');
+                        } else {
+                            webNoted.trigger('shareLinkError');
+                        }
+                    } catch (e) {
+                        webNoted.trigger('shareLinkError');
+                    }
+                });
+            return this;
+        },
 
-		getCurrentDocument: function() {
-			var currentDocument = dataStore.getItem('currentDocument');
-			if (currentDocument === null) {
-				currentDocument = webNoted.webNoted('getNewNoteName');
-				webNoted.webNoted('setCurrentDocument', currentDocument);
-			}
-			return currentDocument;
-		},
+        getCurrentDocument:function () {
+            var currentDocument = dataStore.getItem('currentDocument');
+            if (currentDocument === null) {
+                currentDocument = webNoted.webNoted('getNewNoteName');
+                webNoted.webNoted('setCurrentDocument', currentDocument);
+            }
+            return currentDocument;
+        },
 
-		setCurrentDocument: function(documentName) {
-			dataStore.setItem('currentDocument', documentName);
-		},
+        setCurrentDocument:function (documentName) {
+            dataStore.setItem('currentDocument', documentName);
+        },
 
-		getNewNoteName: function() {
-			var now = new Date;
-			
-			return 'note-' + now;
-		},
+        getNewNoteName:function () {
+            var now = new Date;
 
-		getSharedUrl: function() {
-			return sharedUrl;
-		},
+            return 'note-' + now;
+        },
 
-		getContents: function() {
-			return webNoted.val();
-		},
+        getSharedUrl:function () {
+            return sharedUrl;
+        },
 
-		setContents: function(contents) {
-			webNoted.val(contents).trigger('contentChanged');
-			return this;
-		},
+        getContents:function () {
+            return webNoted.val();
+        },
 
-		setEditable: function() {
-			webNoted
-				.removeAttr('readonly')
-				.focus();
-			return this;
-		},
+        setContents:function (contents) {
+            webNoted.val(contents).trigger('contentChanged');
+            return this;
+        },
 
-		getSavedNotes: function() {
-			var savedNotes = [];
-			for (var i = 0; i < dataStore.length; i++) {
-				if (dataStore.key(i) !== null && dataStore.key(i).substring(0,4) === 'note') {
-					savedNotes.push(dataStore.key(i));
-				}
-			}
-			return savedNotes;
-		},
+        setEditable:function () {
+            webNoted
+                .removeAttr('readonly')
+                .focus();
+            return this;
+        },
 
-		count: function() {
-			return webNoted.val().length;
-		},
+        getSavedNotes:function () {
+            var savedNotes = [];
+            for (var i = 0; i < dataStore.length; i++) {
+                if (dataStore.key(i) !== null && dataStore.key(i).substring(0, 4) === 'note') {
+                    savedNotes.push(dataStore.key(i));
+                }
+            }
+            return savedNotes;
+        },
 
-		version: function() {
-			return version;
-		}
-	};
+        count:function () {
+            return webNoted.val().length;
+        },
 
-	$.fn.webNoted = function(method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof method === 'object' || !method) {
-			return methods.init.apply(this, arguments);
-		} else {
-			$.error('Method ' +  method + ' does not exist on jQuery.webNoted');
-		}
-	};
+        version:function () {
+            return version;
+        }
+    };
+
+    $.fn.webNoted = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.webNoted');
+        }
+    };
 })(jQuery);
--- js/webnoted.js
+++ js/webnoted.js
@@ -1,129 +1,131 @@
-$(function() {
-	var webNoted = $("#webpad");
-	var counterElement = $("#char-count").find("span");
-	var sideBarElement = $("#sidebar");
-	var historyElement = $("#history");
-	var shareDialogElement = $("#share-dialog");
+$(function () {
+    var webNoted = $("#webpad");
+    var counterElement = $("#char-count").find("span");
+    var sideBarElement = $("#sidebar");
+    var historyElement = $("#history");
+    var shareDialogElement = $("#share-dialog");
 
-	if (noteId == '') {
-		sideBarElement.find(".shared").hide();
-	} else {
-		sideBarElement.find(".not-shared").hide();
-	}
+    if (noteId == '') {
+        sideBarElement.find(".shared").hide();
+    } else {
+        sideBarElement.find(".not-shared").hide();
+    }
 
-	shareDialogElement.dialog({
-		autoOpen: false,
-		modal: true,
-		resizable: false,
-		draggable: false
-	});
+    shareDialogElement.dialog({
+        autoOpen:false,
+        modal:true,
+        resizable:false,
+        draggable:false
+    });
 
-	webNoted
-		.webNoted({
-			"apiURL": "/share.php",
-			"dataStore": localStorage,
-			"noteId": noteId
-		})
-		.on('resizeWebNoted', function() {
-			webNoted.height($(window).height() - 88);
-			webNoted.width($(window).width() - 298);
-			roundedEdge(webNoted);
-		})
-		.on('shareLinkGenerated', function() {
-			var url = webNoted.webNoted("getSharedUrl");
-			shareDialogElement
-				.find("#success").show()
-				.end()
-				.find("#processing").hide()
-				.end()
-				.find("#error").hide()
-				.end()
-				.find("#shared-url")
-				.attr("readonly", false)
-				.val(url)
-				.focus(function() { this.select(); })
-				.select()
-				.attr("readonly", true);
-		})
-		.on('shareLinkError', function() {
-			shareDialogElement
-				.find("#error").show()
-				.end()
-				.find("#success").hide()
-				.end()
-				.find("#processing").hide();
-		})
-		.on('contentChanged', function() {
-			roundedEdge(webNoted);
-			setTimeout(function () {
-				counterElement.html(webNoted.webNoted("count"));
-			}, 0);
-		})
-		.on('noteCreated', function() {
-			var documentName = webNoted.webNoted("getCurrentDocument");
-			historyElement.append('<option value="' + documentName + '" selected="selected">' + documentName.substring(5) + '</option>');
-		})
-		.trigger("resizeWebNoted")
-		.trigger("contentChanged")
-	;
+    webNoted
+        .webNoted({
+            "apiURL":"/share.php",
+            "dataStore":localStorage,
+            "noteId":noteId
+        })
+        .on('resizeWebNoted', function () {
+            webNoted.height($(window).height() - 88);
+            webNoted.width($(window).width() - 298);
+            roundedEdge(webNoted);
+        })
+        .on('shareLinkGenerated', function () {
+            var url = webNoted.webNoted("getSharedUrl");
+            shareDialogElement
+                .find("#success").show()
+                .end()
+                .find("#processing").hide()
+                .end()
+                .find("#error").hide()
+                .end()
+                .find("#shared-url")
+                .attr("readonly", false)
+                .val(url)
+                .focus(function () {
+                    this.select();
+                })
+                .select()
+                .attr("readonly", true);
+        })
+        .on('shareLinkError', function () {
+            shareDialogElement
+                .find("#error").show()
+                .end()
+                .find("#success").hide()
+                .end()
+                .find("#processing").hide();
+        })
+        .on('contentChanged', function () {
+            roundedEdge(webNoted);
+            setTimeout(function () {
+                counterElement.html(webNoted.webNoted("count"));
+            }, 0);
+        })
+        .on('noteCreated', function () {
+            var documentName = webNoted.webNoted("getCurrentDocument");
+            historyElement.append('<option value="' + documentName + '" selected="selected">' + documentName.substring(5) + '</option>');
+        })
+        .trigger("resizeWebNoted")
+        .trigger("contentChanged")
+    ;
 
-	$.each(webNoted.webNoted("getSavedNotes"), function(key, value) {
-		var currentDocument = webNoted.webNoted("getCurrentDocument");
-		historyElement.append('<option value="' + value + '">' + value.substring(5) + '</option>');
-		if (currentDocument === value) {
-			historyElement.val(value).attr("selected", true);
-		}
-	});
-	historyElement.on("change", function() {
-		webNoted.webNoted("switchDocument", $(this).val());
-	});
+    $.each(webNoted.webNoted("getSavedNotes"), function (key, value) {
+        var currentDocument = webNoted.webNoted("getCurrentDocument");
+        historyElement.append('<option value="' + value + '">' + value.substring(5) + '</option>');
+        if (currentDocument === value) {
+            historyElement.val(value).attr("selected", true);
+        }
+    });
+    historyElement.on("change", function () {
+        webNoted.webNoted("switchDocument", $(this).val());
+    });
 
-	sideBarElement.find(".jsManageContents").click(function() {
-		var e = $(this);
-		var status = $("#status-message");
-		var statusText = '';
-		var action = e.attr("id");
-		var timeout = 1000;
+    sideBarElement.find(".jsManageContents").click(function () {
+        var e = $(this);
+        var status = $("#status-message");
+        var statusText = '';
+        var action = e.attr("id");
+        var timeout = 1000;
 
-		if (action === "create") {
-			statusText = "New note created";
-		} else if (action === "save") {
-			statusText = "Contents saved";
-		} else if (action === "clear") {
-			statusText = "Contents cleared";
-		} else if (action === "share") {
-			statusText = "Generating Link";
-			shareDialogElement
-				.dialog("open")
-				.find("#processing").show()
-				.end()
-				.find("#success").hide()
-				.end()
-				.find("#error").hide();
-		}
+        if (action === "create") {
+            statusText = "New note created";
+        } else if (action === "save") {
+            statusText = "Contents saved";
+        } else if (action === "clear") {
+            statusText = "Contents cleared";
+        } else if (action === "share") {
+            statusText = "Generating Link";
+            shareDialogElement
+                .dialog("open")
+                .find("#processing").show()
+                .end()
+                .find("#success").hide()
+                .end()
+                .find("#error").hide();
+        }
 
-		webNoted.webNoted(action);
-		status.text(statusText).show("fast");
+        webNoted.webNoted(action);
+        status.text(statusText).show("fast");
 
-		setTimeout(function () {
-			status.hide("fast");
-		}, timeout);
-	});
+        setTimeout(function () {
+            status.hide("fast");
+        }, timeout);
+    });
 
-	$(window)
-		.resize(function() {
-			webNoted.trigger("resizeWebNoted");
-		})
-		.on("blur unload", function() {
-			webNoted.webNoted("save");
-		})
-	;
+    $(window)
+        .resize(function () {
+            webNoted.trigger("resizeWebNoted");
+        })
+        .on("blur unload", function () {
+            webNoted.webNoted("save");
+        })
+    ;
 });
 function roundedEdge(e) {
-	var webNotedElement = e.get(0);
-	if (webNotedElement.scrollHeight > webNotedElement.clientHeight) {
-		e.css("border-radius", "4px 0 0 4px");
-	} else {
-		e.css("border-radius", "4px");
-	}
+    var webNotedElement = e.get(0);
+    if (webNotedElement.scrollHeight > webNotedElement.clientHeight) {
+        e.css("border-radius", "4px 0 0 4px");
+    } else {
+        e.css("border-radius", "4px");
+    }
 }
