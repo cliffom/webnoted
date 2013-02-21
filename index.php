1a0aa230d72211ad102cdc2a3b7e0e80b0d8ec6c
Michael Clifford <cliffom@gmail.com>
2013-02-20 20:45:21 -0800

    Code formatting

--- index.php
+++ index.php
@@ -4,29 +4,29 @@ $noteId = (isset($_GET['noteId'])) ? $_GET['noteId'] : null;
 $note = '';
 
 if ($noteId !== null) {
-	$apiData = @file_get_contents($apiURL . $noteId);
+    $apiData = @file_get_contents($apiURL . $noteId);
 
-	if ($apiData !== false) {
-		$apiData = json_decode($apiData);
+    if ($apiData !== false) {
+        $apiData = json_decode($apiData);
 
-		if (isset($apiData->status)) {
+        if (isset($apiData->status)) {
 
-			if ($apiData->status === 'success') {
-				$note = $apiData->result;
-			} else {
-				header('HTTP/1.0 404 Not Found');
-				die('Error: Invalid noteId');
-			}
+            if ($apiData->status === 'success') {
+                $note = $apiData->result;
+            } else {
+                header('HTTP/1.0 404 Not Found');
+                die('Error: Invalid noteId');
+            }
 
-		} else {
-			header('HTTP/1.0 500 Internal Server Error');
-			die('Fatal Error: API is offline');
-		}
+        } else {
+            header('HTTP/1.0 500 Internal Server Error');
+            die('Fatal Error: API is offline');
+        }
 
-	} else {
-		header('HTTP/1.0 500 Internal Server Error');
-		die('Fatal Error: API URL is unreachable');
-	}
+    } else {
+        header('HTTP/1.0 500 Internal Server Error');
+        die('Fatal Error: API URL is unreachable');
+    }
 } else {
 
 }
