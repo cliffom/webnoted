6215f5aa87c48b76320d05a360e06f3625003972
Michael Clifford <mclifford@luxurylink.com>
2013-03-05 12:14:26 -0800

    shared.php now uses curl

--- php/shared.php
+++ php/shared.php
@@ -2,36 +2,59 @@
 $apiURL = "http://api.webnoted.com/?noteId=";
 $noteId = (isset($_GET['noteId'])) ? $_GET['noteId'] : null;
 $note = '';
-setcookie('noteId', '', time() - 3600);
 
-if ($noteId !== null) {
-    $apiData = @file_get_contents($apiURL . $noteId);
-
+if (!empty($noteId)) {
+    $apiData = getSharedNoteData($apiURL . $noteId);
+ 
     if ($apiData !== false) {
-        $apiData = json_decode($apiData);
-
-        if (isset($apiData->status)) {
-
-            if ($apiData->status === 'success') {
-                setcookie('noteId', $noteId);
-                $note = $apiData->note;
-                $title = (isset($apiData->title)) ? $apiData->title : 'Shared Note';
-            } else {
-                header("Location: /");
-                die();
-            }
-
-        } else {
-            header('HTTP/1.0 500 Internal Server Error');
-            die('Fatal Error: API is offline');
-        }
-
+        $note = $apiData->note;
+        $title = (isset($apiData->title)) ? $apiData->title : 'Shared Note';
+        require 'html/shared.html';
+        die();
     } else {
+        /**
+         * TODO: handle possible scenarios
+         *
+         * 1. Invalid note id - Error page
+         * 2. API unreachable/offline - We are sorry page
+         */
         header('HTTP/1.0 500 Internal Server Error');
-        die('Fatal Error: API URL is unreachable');
+        die('Invalid noteId or API is offline');
     }
 } else {
-
+    header('Location: /');
 }
 
-require 'html/shared.html';
+die();
+
+/**
+ *
+ */
+function getSharedNoteData($apiURL)
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER  => 1,
+        CURLOPT_URL             => $apiURL,
+        CURLOPT_USERAGENT       => 'API Request from WebNoted.com'
+    ));
+
+    $response = curl_exec($curl);
+    $responseInfo = curl_getinfo($curl);
+    curl_close($curl);
+
+    if (
+        $responseInfo['http_code'] === 200
+        && $responseInfo['content_type'] === 'application/json'
+    ) {
+        $response = json_decode($response);
+        if (
+            isset($response->status)
+            && $response->status === 'success'
+        ) {
+            return $response;
+        }
+    }
+    return false;
+}
